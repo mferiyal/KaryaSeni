dd5ffd5cb99845a1977a294a0aeb7b268d4add22
mferiyal <44905071+mferiyal@users.noreply.github.com>
2019-10-28 11:28:45 +0700

    SignInFirebase

--- app/src/main/java/org/jplas/android/karyaseni/MainActivity.java
+++ app/src/main/java/org/jplas/android/karyaseni/MainActivity.java
@@ -1,13 +1,63 @@
 package org.jplas.android.karyaseni;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    View tampilan;
+    EditText inputemail, inputpass;
+    Button buttonSignIn;
+    String TAG = "tag";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inputemail = tampilan.findViewById(R.id.editLoginEmail);
+        inputpass = tampilan.findViewById(R.id.editLoginPassword);
+        buttonSignIn = tampilan.findViewById(R.id.buttonSignIn);
+        mAuth = FirebaseAuth.getInstance();
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = inputemail.getText().toString();
+                String password = inputpass.getText().toString();
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@Nullable Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    FirebaseUser user = mAuth.getCurrentUser();
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+
+                            }
+                        });
+            }
+            });
     }
 }
